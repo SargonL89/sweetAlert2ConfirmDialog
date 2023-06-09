9694dc0adb90e1259afaeebf9e06317cb3f3a40a
Leandro Magnotti <leandro.magnotti@gmail.com>
2023-06-09 07:31:31 -0300

    trayingToDestroyTheSelected

--- javascript.js
+++ javascript.js
@@ -1,60 +1,8 @@
-// function cargarProductos() {
-//   const tableBody = document.querySelector(".body");
-
-// }
-
-// function eliminarElem() {
-//   const buttons = document.querySelectorAll(".eliminar");
-//   buttons.forEach((button) => {
-//     button.addEventListener("click", () => {
-//       const swalWithBootstrapButtons = Swal.mixin({
-//         customClass: {
-//           confirmButton: "btn btn-success",
-//           cancelButton: "btn btn-danger",
-//         },
-//         buttonsStyling: false
-//       });
-
-//       swalWithBootstrapButtons
-//         .fire({
-//           title: "Seguro que querés eliminar este elemento?",
-//           text: "No podrás revertir esta acción!",
-//           icon: "warning",
-//           showCancelButton: true,
-//           confirmButtonText: "Sí, eliminarlo!",
-//           cancelButtonText: "No, cancelar!",
-//           reverseButtons: true
-//         })
-//         .then((result) => {
-//         result.isConfirmed ?
-//             (swalWithBootstrapButtons.fire(
-//               "Listo!",
-//               "El elemento ha sido eliminado!",
-//               "success"
-//             )) :
-//         result.dismiss === Swal.DismissReason.cancel
-//             (swalWithBootstrapButtons.fire(
-//               "Eso estuvo cerca!",
-//               "Tu placa de video imaginaria está a salvo! :)",
-//               "error"
-//             ));
-//         });
-//     });
-//   });
-// }
-
-// cargarProductos();
-// eliminarElem();
-
-// placasDeVideo.forEach((placa, index) => {
-//   const li = document.createElement("li");
-//   li.textContent = placa.id + ' ' + placa.fabricante + ' ' + placa.marca + ' ' + placa.modelo + ' ' + placa.memoria + ' ' + placa.precio;
-
 function kreate() {
   const tableBody = document.querySelector(".tableBody");
   tableBody.innerHTML = "";
 
-  placasDeVideo.forEach((placa, index) => {
+  placasDeVideo.forEach((placa) => {
     tableBody.innerHTML += `<tr class="tr">
         <td>${placa.id}</td>
         <td>${placa.fabricante}</td>
@@ -63,12 +11,57 @@ function kreate() {
         <td>${placa.memoria}</td>
         <td>${placa.precio}</td>
         </tr>`;
-    tableBody.addEventListener("click", () => {
-        placasDeVideo.forEach((placa) => placa.classList.remove("selected"));
-        placa.classList.add("selected");
+  });
+  const selected = document.querySelectorAll(".tr");
+  selected.forEach(row => {
+    row.addEventListener("click", () => {
+      selected.forEach(row => row.classList.remove("selected"));
+      row.classList.add("selected");
+      kreateNDestroy();
     });
   });
 }
+
+function kreateNDestroy() {
+  const eliminate = document.querySelector(".eliminate");
+  const select = document.querySelector(".selected");
+  eliminate.addEventListener("click", () => {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger",
+      },
+      buttonsStyling: false
+    });
+
+    swalWithBootstrapButtons
+      .fire({
+        title: "Seguro que querés eliminar este elemento?",
+        text: "No podrás revertir esta acción!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sí, eliminarlo!",
+        cancelButtonText: "No, cancelar!",
+        reverseButtons: true
+      }).then((result) => {
+      result.isConfirmed ?
+          (swalWithBootstrapButtons.fire(
+            "Listo!",
+            "El elemento ha sido eliminado!",
+            "success"
+          ))
+          eliminarElemento();
+      }); :
+      result.dismiss === Swal.DismissReason.cancel
+          (swalWithBootstrapButtons.fire(
+            "Eso estuvo cerca!",
+            "Tu placa de video imaginaria está a salvo! :)",
+            "error"
+          ));
+      });
+}
+
+
 
 //     const eliminarButton = document.createElement("button");
 //     eliminarButton.textContent = "Eliminar";
@@ -101,19 +94,3 @@ function kreate() {
 // }
 
 kreate();
-
-const listItems = document.querySelectorAll("#myList button");
-
-function Nuevo() {
-  // Agregar el evento de clic a cada elemento de la lista
-  listItems.forEach((item) => {
-    item.addEventListener("click", () => {
-      // Eliminar la clase 'selected' de todos los elementos
-      listItems.forEach((item) => item.classList.remove("selected"));
-
-      // Agregar la clase 'selected' al elemento seleccionado
-      item.classList.add("selected");
-    });
-  });
-}
-Nuevo();
